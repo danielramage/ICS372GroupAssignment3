8538e8b2a5b7763bc42b4d098fc3bb67cde8e053
jritzeku <jritzeku@gmail.com>
2020-04-26 18:16:13 -0500

    added new activities

---
+++ app/src/main/java/edu/metrostate/app/AddWarehouseActivity.java
@@ -0,0 +1,32 @@
+package edu.metrostate.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AddWarehouseActivity extends AppCompatActivity {
+
+
+    private Button home;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_warehouse);
+
+        home = (Button) findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                homeActivity();
+            }
+        });
+    }
+
+    public void homeActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
